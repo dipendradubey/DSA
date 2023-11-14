b9b9e1f3d99cb36a54e6e78e51f0fd8a1ccfa252
Dipendra Dubey <dipendradubey@gmail.com>
2023-11-14 13:18:58 +0530

    Added lecture4 file

---
+++ lecture4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 14;
+    for (int i=1;i<=10;i++){
+        cout<<num*i<<endl;
+    }
+    return 0;
+}
