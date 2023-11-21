221009c4817618b47a5256d5bd7e338c53b2339c
Dipendra Dubey <dipendradubey@gmail.com>
2023-11-21 22:41:44 +0530

    Worked on hard pattern

--- Pattern/Lecture9.cpp
+++ Pattern/Lecture9.cpp
@@ -5,10 +5,12 @@ void pattern1(int n);
 void pattern2(int n);
 void pattern3(int n);
 void pattern4(int n);
+void pattern5(int n);
+void pattern6(int n);
 int main(){
     //pattern1(9);
     //pattern2(5);
-    pattern4(7);
+    pattern6(4);
     return 0;
 }
 void pattern1(int n){
@@ -111,4 +113,75 @@ void pattern4(int n){
     }
     cout<<endl;
    } 
+}
+
+void pattern5(int n){
+    /*
+    *      *
+    **    **
+    ***  ***
+    ********
+    ***  ***
+    **    **
+    *      *
+    */
+
+   for(int row=1; row<=n; row++){
+    for (int col=1; col<=row; col++){
+        cout<<"*";
+    }
+    for (int space=1; space<=2*(n-row); space++){
+        cout<<" ";
+    }
+    for (int col=1; col<=row; col++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+
+   for(int row=1; row<=n-1; row++){
+    for (int col=1; col<=n-row; col++){
+        cout<<"*";
+    }
+    for (int space=1; space<=2*row; space++){
+        cout<<" ";
+    }
+    for (int col=1; col<=n-row; col++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+}
+
+
+void pattern6(int n){
+    /*
+       *
+      *  *
+     *  *  *
+    *  *  *  *
+    *  *  *  *
+     *  *  *
+      *  *
+       * 
+    */
+
+   for (int row=1; row<=n; row++){
+    for (int space=1; space<=n-row;space++){
+        cout<<" ";
+    }
+    for (int col=1; col<=row; col++){
+        cout<<"* ";
+    }
+    cout<<endl;
+   }
+   for (int row=1; row<=n; row++){
+    for (int space=1; space<=row-1;space++){
+        cout<<" ";
+    }
+    for (int col=1; col<=n-(row-1); col++){
+        cout<<"* ";
+    }
+    cout<<endl;
+   }
 }
