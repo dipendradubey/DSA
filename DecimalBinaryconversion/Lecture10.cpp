7e6e7c2717ffbacde00a2d0edcd7c6d52f405c23
Dipendra Dubey <dipendradubey@gmail.com>
2023-11-26 10:41:42 +0530

    completed binary to decimal & vice versa

--- DecimalBinaryconversion/Lecture10.cpp
+++ DecimalBinaryconversion/Lecture10.cpp
@@ -2,8 +2,17 @@
 #include<math.h>
 using namespace std;
 void decimalToBinary();
+void binaryToDecimal();
+void decimalToOcta();
+void octaToDecimal();
+void reverseNum();
+
 int main(){
-    decimalToBinary();
+    //decimalToBinary();
+    //reverseNum();
+    //binaryToDecimal();
+    //decimalToOcta();
+    //octaToDecimal();
     reverseNum();
     return 0;
 }
@@ -11,24 +20,67 @@ int main(){
 void decimalToBinary(){
     int num = 2;
     int decimalNum = 0;
-    int power = 0;
+    int multiply = 1;
     while(num>0){
         int rem = num%2;
         num = num/2;
-        decimalNum = pow(10, power)*rem+decimalNum;
-        power++;
+        decimalNum = multiply*rem+decimalNum;
+        multiply*=10;
     }
     cout<<decimalNum;
 }
 
-void reverseNum(){
-    int n = 123;
+void binaryToDecimal(){
+    int binaryNo = 10;
+    int multiply = 1;
     int ans = 0;
-    int power = 0;
+    while(binaryNo > 0){
+        int rem = binaryNo%10;
+        binaryNo = binaryNo/10;
+        ans = rem*multiply + ans;
+        multiply *= 2;
+    }
+    cout<<"converted decimal is:"<<ans;
+}
+
+void decimalToOcta()
+{
+    int num = 30;
+    int ans = 0;
+    int multiply = 1;
+    while (num){
+        int rem = num%8;
+        num /= 8;
+        ans = multiply*rem + ans;
+        multiply *= 10;
+    }
+    cout<<endl<<"decimal to octa result is: " <<ans;
+}
+
+void octaToDecimal()
+{
+    int num = 36;
+    int ans = 0;
+    int multiply = 1;
+    while (num){
+        int rem = num%10;
+        num /= 10;
+        ans = multiply*rem + ans;
+        multiply *= 8;
+    }
+    cout<<endl<<"octa to decimal result is: " <<ans;
+}
+
+void reverseNum()
+{
+    int n = 12300;
+    int ans = 0;
+    int multiply = 10;
     while(n > 0){
         int rem = n%10; //3 2
         n = n/10; //12 1
-        ans = rem*pow(10, power) + ans; //3
-        power++;
+        ans = ans*multiply + rem; //3
+        //multiply*=10;
     }
+    cout<<"reverse no is: "<<ans<<endl;
 }
