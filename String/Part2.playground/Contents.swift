6d0a374d566882ac8c04517b2d90a89086f58695
Dipendra Dubey <dipendradubey@gmail.com>
2024-06-08 10:34:59 +0530

    solved string problems

--- String/Part2.playground/Contents.swift
+++ String/Part2.playground/Contents.swift
@@ -20,20 +20,66 @@ func checkString(_ s1:String, _ s2:String)->Bool{
     let count = s1.count
     if count != s2.count{return false}
     
-    rotateClockWise()
+    var arrChar = Array(s1)
+    for _ in 1...2{
+        rotateClockWise()
+    }
     
     func rotateClockWise(){
-        var arrChar = Array(s1)
         let tempChar = arrChar[count-1]
-        for index in stride(from: count-2, to: 0, by: -1){
+        for index in stride(from: count-2, through: 0, by: -1){
             arrChar[index+1] = arrChar[index]
         }
         arrChar[0] = tempChar
-        print(s1)
+        print(arrChar)
+    }
+    if String(arrChar) == s2{
+        return true
+    }
+    
+    arrChar = Array(s1)
+    for _ in 1...2{
+        rotateAntiClockWise()
+    }
+    func rotateAntiClockWise(){
+        let tempChar = arrChar[0]
+        for index in stride(from: 0, through: count-2, by: 1){
+            arrChar[index] = arrChar[index+1]
+        }
+        arrChar[count-1] = tempChar
+        print(arrChar)
+    }
+    
+    print(String(arrChar), s2)
+    if String(arrChar) == s2{
+        return true
+    }
+    return false
+}
+
+//print (checkString("ram", "mra"))
+//Check pangram i.e all alphabet are into string or not
+
+func checkPangram()->Bool{
+    let data = "thequickbrownfoxjumpsoverthelazydog"
+    var arrFound = Array(repeating: 0, count: 26)
+    
+    for char in data{
+        let index = Int(char.asciiValue ?? 0)-97
+        print(index)
+        if index >= 0 && index <= 25{
+            arrFound[index] = 1
+        }
+    }
+    print (arrFound)
+    for dataValue in arrFound{
+        if dataValue == 0{
+            return false
+        }
     }
     return true
 }
 
-checkString("ram", "mar")
-
+let x = checkPangram()
+print (x)
 
