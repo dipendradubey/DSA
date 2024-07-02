1784d5fdc9a93bcabb88d7dae80bff4842fb07f1
Dipendra Dubey <dipendradubey@gmail.com>
2024-07-02 07:58:40 +0530

    Worked on kmp algorithm

--- String/KMP/Part43.playground/Contents.swift
+++ String/KMP/Part43.playground/Contents.swift
@@ -40,11 +40,22 @@ func strStr(_ haystack: String, _ needle: String) -> Int {
     var first = 0, second = 0
     var arrLPS:[Int] = Array(repeating: 0, count: needle.count)
     getLPS(&arrLPS, arrNeedleStack)
-    print(arrLPS)
     
     while first<n && second<m{
-        if arrHayStack[]
+        if arrHayStack[first] == arrNeedleStack[second]{
+            first+=1
+            second+=1
+        }else{
+            if second == 0{
+                first+=1
+            }else{
+                second = arrLPS[second-1]
+            }
+        }
         
+    }
+    if second==m{
+        return first-second
     }
     
     return -1
@@ -65,11 +76,11 @@ func getLPS(
                 arrLPS[suf] = 0
                 suf+=1
             }else{
-                pre = arrLPS[suf-1]
+                pre = arrLPS[pre-1]
             }
         }
     }
 }
 
-print(strStr("abcdef", "abca"))
+print(strStr("mississippi", "issip"))
 print("hello")
