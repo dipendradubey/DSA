133746c9ab79a879da451ad94472e80c369c58de
Dipendra Dubey <dipendradubey@gmail.com>
2024-07-04 07:55:57 +0530

    Worked on kmp problem

--- String/KMP/Part44.playground/Contents.swift
+++ String/KMP/Part44.playground/Contents.swift
@@ -1,11 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Hello"
 
 //Geek for geeks
 //min charater to be added at front to make string palindrome
 func minChar(_ str:String)->Int{
     var arrStr = Array(str)
+    
     var first = 0, last = str.count - 1
     while first<last{
         let temp = arrStr[first]
@@ -15,11 +16,35 @@ func minChar(_ str:String)->Int{
         last-=1
     }
     arrStr = Array(str) + ["#"] + arrStr
-    print(arrStr)
-    return 0
+    var arrLPS = Array(repeating: 0, count: arrStr.count)
+    getLps(for: arrStr, for: &arrLPS)
+    print("lps table \(arrLPS)")
+    //print("arrstring \(arrStr)")
+    return str.count - (arrLPS.last ?? 0)
 }
 
 
+func getLps(for arr:[Character], for arrLPS:inout [Int]){
+    var pre = 0, suf = 1
+    while suf<arrLPS.count{
+        if arr[pre] == arr[suf]{
+            arrLPS[suf] = pre+1
+            suf+=1
+            pre+=1
+        }else{
+            if pre==0{
+                arrLPS[suf] = 0
+                suf+=1
+            }else{
+                pre = arrLPS[pre-1]
+            }
+        }
+    }
+}
 
-print(greeting)
-minChar("RAMS")
+print(minChar("ROORSP"))
+///print(greeting)
+
+
+
+print (minChar("ABAC"))
