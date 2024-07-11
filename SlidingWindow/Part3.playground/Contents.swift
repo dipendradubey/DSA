4a845ae31517617b85023693430c09d235b0ffb1
Dipendra Dubey <dipendradubey@gmail.com>
2024-07-11 07:52:36 +0530

    Solved leetcode 913 problem

--- SlidingWindow/Part3.playground/Contents.swift
+++ SlidingWindow/Part3.playground/Contents.swift
@@ -46,10 +46,26 @@ func subarraysProductBruteForce(lesserto k: Double, _ array: [Int]) -> Int {
     return count
 }
 
+func numSubarrayProductLessThanK(_ array: [Int], _ k: Int) -> Int {
+        let n = array.count
+         var count = 0
+         var product = 1, start = 0, end = 0
+         while end < n{
+           product *= array[end]
+            while product>=k && start<=end{
+                product/=array[start]
+                start+=1
+            }
+            count += end-start+1
+            end+=1
+        }
+        return count
+    }
+
 let array = [4,32,23,1,11,21,8,3,12,21,33,5,14,11,9,14,4,27,5,7,7,14,13,9,17,10,3,4,17,31,7,19,4,7,20,5,4,3,24,30,9,11,23,16,7,21,23,22,12,20,8,20,18,11,5,8,21,5,11,31,28,21,13,21,22,3,2,1,32,8,8,27,4,26,13,14,4,15,6,10,28,15,18,20,29,1,1,12,5,18,10,9,5,13,1,2,7,3,23,26,3,4,13,9,32,32,31,26,19,8,7,29,16,21,28,14,18,5]
 print("Hello....")
 print(subarraysProductBruteForce(lesserto: 9931, array))
 //print(subarraysDivByK([5], 9))
 
-let i:Int = Int(4.9776983885183386e+19)
+
 
