f0c74580efa48c33692b58a63f7f292e1d52e035
Dipendra Dubey <dipendradubey@gmail.com>
2024-08-21 06:57:52 +0530

    worked on recursion

--- Recursion/Lecture53_Part3.playground/Contents.swift
+++ Recursion/Lecture53_Part3.playground/Contents.swift
@@ -1,11 +1,32 @@
 import UIKit
-
+//Factorial
 print(fact(5))
-
 func fact(_ num:Int)->Int{
     
     if num==0{
         return 1
     }
     return num*fact(num-1)
+}
+
+//Sum of n natural no
+print(sumOfNum(5))
+func sumOfNum(_ num:Int)->Int{
+    if num==1{
+        return num
+    }
+    return num+sumOfNum(num-1)
+    
+}
+
+//power of num to n times
+print(powOfNum(10,0))
+func powOfNum(_ num:Int, _ n:Int)->Int{
+    if n==1{
+        return num
+    }
+    print("fn is callled")
+    return num*powOfNum(num,n-1)
+    
+    
 }
