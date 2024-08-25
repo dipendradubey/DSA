bca9b0605eeea9aab8ccfc24d938a29483e55748
Dipendra Dubey <dipendradubey@gmail.com>
2024-08-25 14:44:18 +0530

    solved recursion problem

--- Recursion/Lect54_Part4.playground/Contents.swift
+++ Recursion/Lect54_Part4.playground/Contents.swift
@@ -9,3 +9,30 @@ func fib(_ n:Int)->Int{
     return fib(n-1)+fib(n-2)
 }
 
+//Leetcode 40
+//This code shows time limit exceed problem
+func climbStairs(_ n: Int) -> Int {
+    if n<=1{
+        return 1
+    }
+    return climbStairs(n-1) + climbStairs(n-2)
+}
+
+//Homework
+func climbStairsForLoop(_ n: Int) -> Int {
+    var totalStep = 1
+    
+    for index in stride(from: 2, through: n, by: 1){
+        totalStep += (index - 1)
+    }
+    return totalStep
+}
+
+print(gcdOfNum(18, 48))
+func gcdOfNum(_ num1:Int, _ num2:Int)->Int{
+    if num2 == 0{
+        return num1
+    }
+    return gcdOfNum(num2, num1 % num2)
+}
+
